18ff679cd83d6417e74d0578d0c8fa75533f9044
chokobo-ume <flente3@gmail.com>
2019-08-25 01:01:29 +0900

    add_ajax

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,5 +1,13 @@
 $(function() {
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
+    $.ajax({
+      url: '/users',
+      type: "GET",
+      data: { keyword: input },
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
   });
 });
