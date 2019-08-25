cc9b7d30251df2367a7aa6c43b822bdeeedfc16f
chokobo-ume <flente3@gmail.com>
2019-08-25 17:07:31 +0900

    add_reload_function

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -45,4 +45,26 @@ $(function(){
       alert('error');
       })
   })
+
+// 自動更新
+    var reloadMessages = function() {
+      //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+      last_message_id = $('.message').last().data('message-id');
+      $.ajax({
+        //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+        url: location.href,
+        //ルーティングで設定した通りhttpメソッドをgetに指定
+        type: 'GET',
+        dataType: 'json',
+        //dataオプションでリクエストに値を含める
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+        console.log('success');
+      })
+      .fail(function() {
+        console.log('error');
+      });
+    };
+
 });
