afb47cfbeddcc41241154e4550e4998d908bd279
chokobo-ume <@users.noreply.github.com>
2019-09-03 22:19:37 +0900

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ lock "~> 3.11.1"
 set :application, "my_app_name"
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url, "git@example.com:me/my_repo.git"
+set :repo_url, 'git@github.com:toshiyuki-umemura/chat-space.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
