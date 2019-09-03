1fb60358920df1a9dfb7edbea33e4935c4f69799
chokobo-ume <@users.noreply.github.com>
2019-09-03 22:34:34 +0900

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,10 +3,10 @@
 lock "~> 3.11.1"
 
 # Capistranoのログの表示に利用する
-set :application, "my_app_name"
+set :application, "chat-space"
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url, 'git@github.com:toshiyuki-umemura/chat-space.git'
+set :repo_url, "git@github.com:toshiyuki-umemura/chat-space.git"
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
